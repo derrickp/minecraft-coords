30782a0957b8622fd3306ca4923f61ffe839469e
Derrick Plotsky <derrickplotsky@gmail.com>
2020-06-03 23:40:53 -0700

    Add firebase config object
    Just a bucket of config.

---
+++ src/config/firebase.ts
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "minecraft-coordinates.firebaseapp.com",
+  databaseURL: "https://minecraft-coordinates.firebaseio.com",
+  projectId: "minecraft-coordinates",
+  storageBucket: "minecraft-coordinates.appspot.com",
+  messagingSenderId: "749285287099",
+  appId: "1:749285287099:web:ea2c874f3b283bb027359e",
+};
