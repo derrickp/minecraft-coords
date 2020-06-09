6a86111bd356d94f1bf5eb136b7bd1c4b94fe8bb
Derrick Plotsky <derrickplotsky@gmail.com>
2020-06-08 21:52:51 -0700

    Move firebase bootstrapping into own file
    I want this in its own place, not in the root like it was previously.
    
    Doing it this way we can add new bootstrap logic as need be here or
    in other separate files.

---
+++ src/bootstrap/firebase.ts
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+
+// Side-effect imports
+import "regenerator-runtime/runtime";
+import "firebase/auth";
+
+import { config } from "../config/firebase";
+
+export function getFirebaseApp() {
+    return firebase.initializeApp(config, "minecraft-coords");
+}
