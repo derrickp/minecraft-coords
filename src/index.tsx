4e97e109b919eca9b5ecba42914cf767fb718049
Derrick Plotsky <derrickplotsky@gmail.com>
2020-06-08 22:00:52 -0700

    Have index use new code locations
    I've moved a bunch of this code to other places, so
    now I can clean up this index file so its doing way less.

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,15 @@
-import firebase from "firebase/app";
-
-// Side-effect imports
-import "regenerator-runtime/runtime";
-import "firebase/auth";
-
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import { config as firebaseConfig } from "~/config/firebase";
-import { fromFirebaseUser, User } from "~security/User";
+import { User } from "~security/User";
 import { App } from "~/App";
+import { signOut, signUp, signIn, subscribeToUserChanges } from "~security/authentication";
 
 const element = document.getElementById("app");
 
 if (!element) {
   console.error("Nothing is going to work now.");
-}
-
-const firebaseApp = firebase.initializeApp(firebaseConfig, "minecraft-coords");
-
-async function signUp(
-  email: string,
-  password: string
-): Promise<firebase.User | null> {
-  const credential = await firebaseApp
-    .auth()
-    .createUserWithEmailAndPassword(email, password);
-  return credential.user;
-}
-
-async function signIn(
-  email: string,
-  password: string
-): Promise<firebase.User | null> {
-  const credential = await firebaseApp
-    .auth()
-    .signInWithEmailAndPassword(email, password);
-  return credential.user;
-}
-
-async function signOut(): Promise<void> {
-  return firebaseApp.auth().signOut();
 }
 
 (window as any)["signout"] = signOut;
@@ -55,14 +23,8 @@ function renderApp(user?: User) {
   );
 }
 
-firebaseApp.auth().onAuthStateChanged((fbUser) => {
-  console.log("auth changed");
-  if (fbUser) {
-    const user = fromFirebaseUser(fbUser);
-    renderApp(user);
-  } else {
-    renderApp();
-  }
+subscribeToUserChanges((user) => {
+  renderApp(user);
 });
 
 renderApp();
