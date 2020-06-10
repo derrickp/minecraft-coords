618d923598395bcda186c02e75f427f44eda1310
Derrick Plotsky <derrickplotsky@gmail.com>
2020-06-10 08:28:48 -0700

    Add VillagerTrades to Coordinate
    This will help a player keep track of the trades available
    at or near the coordinate, if any.

--- src/minecraft/Coordinate.ts
+++ src/minecraft/Coordinate.ts
@@ -1,7 +1,11 @@
+import { VillagerTrade } from "./VillagerTrade";
+
 export interface Coordinate {
   x: number;
   y: number;
   z: number;
   name: string;
+  biome: string;
   tags: string[];
+  villagerTrades: VillagerTrade[];
 }
