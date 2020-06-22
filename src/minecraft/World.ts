157d51f5c6a070fbb6d3ba7922b7af7ff24e5ad0
Derrick Plotsky <derrickplotsky@gmail.com>
2020-06-22 16:54:24 -0700

    Update code for building work objects
    To make it easier to make these things.

--- src/minecraft/World.ts
+++ src/minecraft/World.ts
@@ -1,23 +1,41 @@
 import { Coordinate } from "./Coordinate";
+import { Platform } from "./Platform";
 
 export interface World {
   id: string;
   seed: string;
   name: string;
+  platform: Platform;
   coordinates: Coordinate[];
 }
 
-export function buildWorld(
-  idUniqueifier: string,
-  id: string,
-  seed: string,
-  name: string,
-  coordinates: Coordinate[]
-): World {
+export interface BuildWorldArgs {
+  idUniqueifier: string;
+  id: string;
+  seed: string;
+  name: string;
+  coordinates?: Coordinate[];
+  platform?: Platform;
+}
+
+export function getFriendlyId(id: string): string {
+  const parts = id.split("-");
+  return parts[1];
+}
+
+export function buildWorld({
+  idUniqueifier,
+  id,
+  seed,
+  name,
+  platform = Platform.BEDROCK,
+  coordinates = [],
+}: BuildWorldArgs): World {
   return {
     seed,
     name,
+    platform,
     id: `${idUniqueifier}-${id}`,
-    coordinates: coordinates ? coordinates : [],
+    coordinates: coordinates,
   };
 }
