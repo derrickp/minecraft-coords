7aadf57c3727919874e2852430047e0397bfe530
Derrick Plotsky <derrickplotsky@gmail.com>
2020-06-22 16:51:39 -0700

    Add NewWorldForm
    This is the actual form for creating a new world.
    
    Just the basics.

---
+++ src/components/NewWorldForm.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Platform } from "~minecraft/Platform";
+import { Form, FormField, TextInput, Select, Box, Button } from "grommet";
+
+export interface NewWorldDetails {
+  id: string;
+  name: string;
+  seed: string;
+  platform: Platform;
+}
+
+export interface NewWorldFormProps {
+  onWorldSubmitted: (details: NewWorldDetails) => void;
+  onCancel: () => void;
+}
+
+export const NewWorldForm = (props: NewWorldFormProps): JSX.Element => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [seed, setSeed] = useState("");
+  const [platform, setPlatform] = useState(Platform.BEDROCK);
+
+  const onSubmit = () => {
+    props.onWorldSubmitted({
+      id,
+      name,
+      seed,
+      platform,
+    });
+  };
+
+  return (
+    <Form
+      onReset={() => {
+        setId("");
+        setName("");
+        setSeed("");
+      }}
+      onSubmit={onSubmit}
+    >
+      <FormField
+        label="World ID (A helpful reference identifier for you)"
+        name="id"
+      >
+        <TextInput
+          name="id"
+          value={id}
+          onChange={(event) => setId(event.target.value)}
+        />
+      </FormField>
+      <FormField label="World Name" name="name">
+        <TextInput
+          name="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </FormField>
+      <FormField label="World Seed" name="seed">
+        <TextInput
+          name="seed"
+          value={seed}
+          onChange={(event) => setSeed(event.target.value)}
+        />
+      </FormField>
+      <FormField label="Platform" name="platform">
+        <Select
+          name="platform"
+          options={[Platform.BEDROCK, Platform.JAVA]}
+          value={platform}
+          onChange={(event) => setPlatform(event.option)}
+        />
+      </FormField>
+      <Box direction="row" justify="between" margin={{ top: "medium" }}>
+        <Button label="Cancel" onClick={props.onCancel} />
+        <Button type="reset" label="Reset Form" />
+        <Button type="submit" label="Save World" primary />
+      </Box>
+    </Form>
+  );
+};
