91375d872c27210bb2f0808c2da02e9ca1d047c7
Derrick Plotsky <derrickplotsky@gmail.com>
2020-06-22 16:45:41 -0700

    Make NewWorld page
    This houses the form for making a new world.
    
    It also calls the code to actually save the world to the server.

---
+++ src/pages/NewWorld.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { buildWorld } from "~minecraft/World";
+import { Box } from "grommet";
+import { User } from "~User";
+import { NewWorldForm, NewWorldDetails } from "~components/NewWorldForm";
+import { useHistory } from "react-router-dom";
+import { saveNewWorld } from "~firebase_data/worlds";
+
+export interface NewWorldProps {
+  user: User;
+}
+
+export const NewWorld = (props: NewWorldProps): JSX.Element => {
+  const history = useHistory();
+  const [isSavingNewWorld, setSavingNewWorld] = useState(false);
+
+  const onCancel = () => {
+    history.push("/");
+  };
+
+  const onWorldSubmitted = async (details: NewWorldDetails) => {
+    const world = buildWorld({
+      idUniqueifier: props.user.id,
+      id: details.id,
+      seed: details.seed,
+      name: details.name,
+    });
+    setSavingNewWorld(true);
+    const result = await saveNewWorld(world, props.user);
+    console.log(result);
+    history.push("/");
+  };
+
+  return (
+    <Box fill align="center" justify="center">
+      <Box width="medium">
+        {isSavingNewWorld ? (
+          <div>Loading...</div>
+        ) : (
+          <NewWorldForm
+            onCancel={onCancel}
+            onWorldSubmitted={onWorldSubmitted}
+          ></NewWorldForm>
+        )}
+      </Box>
+    </Box>
+  );
+};
