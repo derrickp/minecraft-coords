5e158ae983809f5e1e8b00209236883b5c351c7b
Derrick Plotsky <derrickplotsky@gmail.com>
2020-07-07 22:39:43 -0700

    Fixup some of the add coordinate form
    Just to make this look better when it's rendered.

--- src/components/AddCoordinateForm.tsx
+++ src/components/AddCoordinateForm.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
-import { Form, FormField, TextInput, Box, Button, CheckBox } from "grommet";
+import {
+  Form,
+  FormField,
+  TextInput,
+  Box,
+  Button,
+  CheckBox,
+  Heading,
+} from "grommet";
 import { NewCoordinateDetails } from "./NewCoordinateDetails";
 
 export interface AddCoordinateFormProps {
+  worldName: string;
   onCoordinateSubmitted: (coordiante: NewCoordinateDetails) => void;
   onCancel: () => void;
 }
@@ -31,71 +40,74 @@ export const AddCoordinateForm = (
   };
 
   return (
-    <Form
-      onReset={() => {
-        setX("");
-        setY("");
-        setZ("");
-        setTags([]);
-        setHasVillage(false);
-      }}
-      onSubmit={onSubmit}
-    >
-      <FormField label="Name" name="name">
-        <TextInput
-          name="Name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </FormField>
-      <FormField label="X coordinate" name="x">
-        <TextInput
-          name="X"
-          value={x}
-          onChange={(event) => setX(event.target.value)}
-        />
-      </FormField>
-      <FormField label="Y coordinate" name="y">
-        <TextInput
-          name="y"
-          value={y}
-          onChange={(event) => setY(event.target.value)}
-        />
-      </FormField>
-      <FormField label="Z coordinate" name="z">
-        <TextInput
-          name="z"
-          value={z}
-          onChange={(event) => setZ(event.target.value)}
-        />
-      </FormField>
-      <FormField label="Biome" name="biome">
-        <TextInput
-          name="biome"
-          value={biome}
-          onChange={(event) => setBiome(event.target.value)}
-        />
-      </FormField>
-      <FormField name="hasVillageNearby">
-        <CheckBox
-          name="hasVillageNearby"
-          label="Has Village Nearby?"
-          checked={hasVillageNearby}
-          onChange={(event) => setHasVillage(event.target.checked)}
-        />
-      </FormField>
-      <FormField label="Tags (separate by ,)" name="tags">
-        <TextInput
-          name="tags"
-          value={tags.join(",")}
-          onChange={(event) => setTags(event.target.value.split(","))}
-        />
-      </FormField>
-      <Box direction="row" justify="between" margin={{ top: "medium" }}>
-        <Button label="Cancel" onClick={props.onCancel} />
-        <Button type="reset" label="Reset Form" />
-        <Button type="submit" label="Save World" primary />
-      </Box>
-    </Form>
+    <Box>
+      <Heading level={4}>{`New coordinate for ${props.worldName}`}</Heading>
+      <Form
+        onReset={() => {
+          setX("");
+          setY("");
+          setZ("");
+          setTags([]);
+          setHasVillage(false);
+        }}
+        onSubmit={onSubmit}
+      >
+        <FormField label="Coordinate Name" name="name">
+          <TextInput
+            name="Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </FormField>
+        <FormField label="X" name="x">
+          <TextInput
+            name="X"
+            value={x}
+            onChange={(event) => setX(event.target.value)}
+          />
+        </FormField>
+        <FormField label="Y" name="y">
+          <TextInput
+            name="y"
+            value={y}
+            onChange={(event) => setY(event.target.value)}
+          />
+        </FormField>
+        <FormField label="Z" name="z">
+          <TextInput
+            name="z"
+            value={z}
+            onChange={(event) => setZ(event.target.value)}
+          />
+        </FormField>
+        <FormField label="Biome" name="biome">
+          <TextInput
+            name="biome"
+            value={biome}
+            onChange={(event) => setBiome(event.target.value)}
+          />
+        </FormField>
+        <FormField name="hasVillageNearby">
+          <CheckBox
+            name="hasVillageNearby"
+            label="Has Village Nearby?"
+            checked={hasVillageNearby}
+            onChange={(event) => setHasVillage(event.target.checked)}
+          />
+        </FormField>
+        <FormField label="Tags (separate by ,)" name="tags">
+          <TextInput
+            name="tags"
+            value={tags.join(",")}
+            onChange={(event) => setTags(event.target.value.split(","))}
+          />
+        </FormField>
+        <Box direction="row" justify="between" margin={{ top: "medium" }}>
+          <Button label="Cancel" onClick={props.onCancel} />
+          <Button type="reset" label="Reset" />
+          <Button type="submit" label="Save Coordinate" primary />
+        </Box>
+      </Form>
+    </Box>
   );
 };
