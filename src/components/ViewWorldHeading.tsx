b8c90eb819369988b86ed7eb8b67c7759562d20f
Derrick Plotsky <derrickplotsky@gmail.com>
2020-07-07 22:35:41 -0700

    Add ViewWorldHeading
    The heading for our ViewWorld page.

---
+++ src/components/ViewWorldHeading.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { World } from "~minecraft/World";
+import { Box, Heading, Text } from "grommet";
+
+export interface ViewWorldHeadingProps {
+  world: World;
+}
+
+export const ViewWorldHeading = (props: ViewWorldHeadingProps): JSX.Element => {
+  const { world } = props;
+  return (
+    <>
+      <Box margin="small" justify="start">
+        <Heading level={4}>World Name</Heading>
+        <Text>{world.name}</Text>
+      </Box>
+      <Box margin="small" justify="start">
+        <Heading level={4}>World Seed</Heading>
+        <Text>{world.seed}</Text>
+      </Box>
+    </>
+  );
+};
