977e56c4f2ea13232933bee3bd44aa345b0e7fab
Derrick Plotsky <derrickplotsky@gmail.com>
2020-07-21 19:36:19 -0700

    Add TradeItem component
    It just shows a trade.

---
+++ src/components/TradeItem.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "grommet";
+
+import { Trade } from "~minecraft/Trade";
+
+export interface TradeItemProps {
+  trade: Trade;
+}
+
+export const TradeItem = (props: TradeItemProps): JSX.Element => {
+  return (
+    <Text size="medium">{`${props.trade.given} -> ${props.trade.received}`}</Text>
+  );
+};
