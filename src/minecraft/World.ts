ac641555d9ffda9f83e4d64dcd4bbf458ab14a89
Derrick Plotsky <derrickplotsky@gmail.com>
2020-07-21 19:36:44 -0700

    Add some coordinate id methods
    For getting the next id, and getting a coordinate by id.

--- src/minecraft/World.ts
+++ src/minecraft/World.ts
@@ -1,6 +1,8 @@
 import { Coordinate } from "./Coordinate";
 import { Platform } from "./Platform";
 
+export type MaybeWorld = World | undefined;
+
 export interface World {
   id: string;
   seed: string;
@@ -24,6 +26,29 @@ export interface BuildWorldArgs {
 export function getFriendlyId(id: string): string {
   const parts = id.split("-");
   return parts[1];
+}
+
+export function getNextCoordinateId(world: World): number {
+  if (!world.coordinates) {
+    return 1;
+  }
+
+  const existingMax = world.coordinates.sort((a, b) => a.id - b.id)[
+    world.coordinates.length - 1
+  ];
+
+  if (existingMax && existingMax.id) {
+    return existingMax.id + 1;
+  }
+
+  return 1;
+}
+
+export function coordinateById(
+  world: World,
+  coordinateId: number
+): Coordinate | undefined {
+  return world.coordinates.find((c) => c.id == coordinateId);
 }
 
 export function buildWorld({
